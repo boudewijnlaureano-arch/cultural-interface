b215e847c594cbb8229e785cdd4e4b54a0b423e2
boudewijnlaureano-arch <boudewijnlaureano@gmail.com>
2025-12-13 18:16:43 +0100

    Centering and fade in information added

--- cultural-interface/src/components/ui/shadcn-io/android/android.tsx
+++ cultural-interface/src/components/ui/shadcn-io/android/android.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import * as React from "react";
 import { SVGProps } from "react";
 
 export interface AndroidProps extends SVGProps<SVGSVGElement> {
@@ -7,6 +8,7 @@ export interface AndroidProps extends SVGProps<SVGSVGElement> {
   height?: number;
   src?: string;
   videoSrc?: string;
+  children?: React.ReactNode;
 }
 
 export const Android = ({
@@ -14,17 +16,23 @@ export const Android = ({
   height = 882,
   src,
   videoSrc,
+  children,
   ...props
 }: AndroidProps) => {
+  // Safe unique id for clipPath (no ":" issues)
+  const rawId = React.useId();
+  const clipId = `android-clip-${rawId.replace(/[^a-zA-Z0-9_-]/g, "")}`;
+
   return (
     <svg
-      width={width}
-      height={height}
       viewBox={`0 0 ${width} ${height}`}
+      preserveAspectRatio="none"
+      style={{ width: "100%", height: "100%", display: "block" }}
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
+      {/* Side buttons */}
       <path
         d="M376 153H378C379.105 153 380 153.895 380 155V249C380 250.105 379.105 251 378 251H376V153Z"
         className="fill-[#E5E5E5] dark:fill-[#404040]"
@@ -33,6 +41,8 @@ export const Android = ({
         d="M376 301H378C379.105 301 380 301.895 380 303V351C380 352.105 379.105 353 378 353H376V301Z"
         className="fill-[#E5E5E5] dark:fill-[#404040]"
       />
+
+      {/* Phone body */}
       <path
         d="M0 42C0 18.8041 18.804 0 42 0H336C359.196 0 378 18.804 378 42V788C378 811.196 359.196 830 336 830H42C18.804 830 0 811.196 0 788V42Z"
         className="fill-[#E5E5E5] dark:fill-[#404040]"
@@ -42,60 +52,61 @@ export const Android = ({
         className="fill-white dark:fill-[#262626]"
       />
 
-      <g clipPath="url(#android-clip)">
+      {/* Screen (everything inside clipped to exact shape) */}
+      <g clipPath={`url(#${clipId})`}>
+        {/* Screen background */}
         <path
           d="M9.25 48C9.25 29.3604 24.3604 14.25 43 14.25H335C353.64 14.25 368.75 29.3604 368.75 48V780C368.75 798.64 353.64 813.75 335 813.75H43C24.3604 813.75 9.25 798.64 9.25 780V48Z"
           className="fill-[#E5E5E5] stroke-[#E5E5E5] stroke-[0.5] dark:fill-[#404040] dark:stroke-[#404040]"
         />
-      </g>
-      <circle
-        cx="189"
-        cy="28"
-        r="9"
-        className="fill-white dark:fill-[#262626]"
-      />
-      <circle
-        cx="189"
-        cy="28"
-        r="4"
-        className="fill-[#E5E5E5] dark:fill-[#404040]"
-      />
-      {src && (
-        <image
-          href={src}
-          width="360"
-          height="800"
-          className="size-full object-cover"
-          preserveAspectRatio="xMidYMid slice"
-          clipPath="url(#android-clip)"
-        />
-      )}
-      {videoSrc && (
-        <foreignObject
-          x="9"
-          y="14"
-          width="360"
-          height="800"
-          clipPath="url(#android-clip)"
-        >
-          <video
-            className="size-full object-cover"
-            src={videoSrc}
-            autoPlay
-            loop
-            muted
-            playsInline
+
+        {/* Optional image background */}
+        {src && (
+          <image
+            href={src}
+            x="9"
+            y="14"
+            width="360"
+            height="800"
+            preserveAspectRatio="xMidYMid slice"
           />
-        </foreignObject>
-      )}
+        )}
+
+        {/* Optional video background */}
+        {videoSrc && (
+          <foreignObject x="9" y="14" width="360" height="800">
+            <video
+              style={{ width: "100%", height: "100%" }}
+              className="object-cover"
+              src={videoSrc}
+              autoPlay
+              loop
+              muted
+              playsInline
+            />
+          </foreignObject>
+        )}
+
+        {/* Your React UI */}
+        {children && (
+          <foreignObject x="9" y="14" width="360" height="800">
+            <div
+              xmlns="http://www.w3.org/1999/xhtml"
+              style={{ width: "100%", height: "100%", display: "block" }}
+            >
+              {children}
+            </div>
+          </foreignObject>
+        )}
+      </g>
+
       <defs>
-        <clipPath id="android-clip">
+        <clipPath id={clipId}>
           <rect
             width="360"
             height="800"
             rx="33"
             ry="25"
-            className="fill-white dark:fill-[#262626]"
             transform="translate(9 14)"
           />
         </clipPath>
